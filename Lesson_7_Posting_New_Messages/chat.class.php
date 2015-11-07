b33e9218cdcb6cbd31899bcacab2893fea8b9d7f
Andrew Menning <menning23@gmail.com>
2015-11-06 22:12:54 -0500

    Lesson 7 Posting New Messages

---
+++ Lesson_7_Posting_New_Messages/chat.class.php
@@ -0,0 +1,36 @@
+<?php
+require_once('config.php');
+require_once('bucky_error_handler.php');
+
+class Chat
+{
+
+	private $mysqli;
+	
+	//constructor open database connection
+	function __construct(){
+		$this->mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
+	}
+	
+	//destructor closes database connection
+	function __destruct(){
+		$this->mysqli->close();
+	}
+	
+	//Truncates (empties) the table containing all messages
+	public function deleteAllMessages(){
+		$query = 'TRUNCATE TABLE chat';
+		$result = $this->mysqli->query($query);
+	}
+	
+	public function postNewMessage($user_name, $message, $color){
+		$user_name = $this->mysqli->real_escape_string($user_name);
+		$message = $this->mysqli->real_escape_string($message);
+		$color = $this->mysqli->real_escape_string($color);
+		$query = 'INSERT INTO chat (posted_on, user_name, message, color)' . ' VALUES ( NOW(), "'.$user_name.'", "'.$message.'", "'.$color.'")';
+		$result = $this->mysqli->query($query);
+	}
+	
+}
+
+?>
